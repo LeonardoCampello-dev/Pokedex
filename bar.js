5b4217853a4f4f94287f2f3e3c06087f214ad251
Leonardo Campello <leonardocampello.dev@gmail.com>
2020-08-16 22:13:55 -0300

    Implemented percentage bar

---
+++ bar.js
@@ -0,0 +1,30 @@
+const container = document.querySelector(".container")
+let bar = document.createElement("div")
+
+function createBarStyle(item) {
+    item.style.height = "4px"
+    item.style.width = "0"
+    item.style.backgroundColor = "#FECA1B"
+    item.style.position = "fixed"
+    item.style.top = "0"
+    item.style.left = "0"
+    item.style.zIndex = "9999"
+    item.style.transition = "0.2s"
+}
+
+function updateBar() {
+    const containerHeight = container.offsetHeight
+    const pagePosition = window.pageYOffset
+
+    const updatedBar = (pagePosition * 100) / containerHeight
+
+    bar.style.width = updatedBar + "%"
+}
+
+document.body.append(bar)
+
+createBarStyle(bar)
+
+window.addEventListener("load", () => {
+    document.addEventListener("scroll", updateBar)
+})
